f5b100ded9302da0b439baba31266cb3a9e02683
fede <fede>
2013-11-20 12:30:51 -0300

    rem

--- server/includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbname="eventos_fce";
-$server="localhost";
-$username="root";
-$password="";
-$db = mysql_connect($server,$username,$password);
-mysql_query("use $dbname");
-mysql_query("SET NAMES utf8");
-?>
