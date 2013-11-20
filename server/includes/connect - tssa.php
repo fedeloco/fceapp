a52dbe1f54cd5d30102531dcedb284388e05fa35
fede <fede>
2013-11-20 13:59:21 -0300

    ss

--- server/includes/connect - tssa.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbname="juc2013";
-$server="localhost";
-$username="root";
-$password="webfce123";
-$db = mysql_connect($server,$username,$password);
-mysql_query("use $dbname");
-mysql_query("SET NAMES utf8");
-?>
